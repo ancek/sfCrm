514853678f8ceb028f4b3af396e4c29e6a5b8b83
Jakub Anckiewicz <jakub.anckiewicz@gmail.com>
2015-11-14 12:07:39 +0100

    Create User Entities

---
+++ src/AppBundle/Entity/UserDetailsClient.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserDetailsClient
+ * 
+ * @ORM\Entity
+ */
+class UserDetailsClient extends UserDetails
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pesel", type="string", length=11)
+     */
+    private $pesel;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set pesel
+     *
+     * @param string $pesel
+     *
+     * @return UserDetailsClient
+     */
+    public function setPesel($pesel)
+    {
+        $this->pesel = $pesel;
+
+        return $this;
+    }
+
+    /**
+     * Get pesel
+     *
+     * @return string
+     */
+    public function getPesel()
+    {
+        return $this->pesel;
+    }
+}
